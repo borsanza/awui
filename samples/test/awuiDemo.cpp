16e6b8c48d591fc36dfc3aa4cee751857199c31d
BorSanZa <borsanza@gmail.com>
2013-06-02 03:00:02 +0200

    Mas cosas en el ignore

---
+++ samples/test/awuiDemo.cpp
@@ -0,0 +1,16 @@
+// (c) Copyright 2011 Borja Sánchez Zamorano (BSD License)
+// feedback: borsanza AT gmail DOT com
+
+#include "form1.h"
+
+#include <awui/Windows/Forms/Application.h>
+
+using namespace awui::Windows::Forms;
+
+int main(int argc, char ** argv) {
+	Form1 *form = new Form1();
+
+	Application::Run(form);
+
+	return 0;
+}
