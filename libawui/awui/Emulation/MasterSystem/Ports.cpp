460193a55681ac9056e6a606bca7940b5392c960
BorSanZa <borsanza@gmail.com>
2014-06-20 16:56:23 +0200

    Agregada la clase de los puertos

---
+++ libawui/awui/Emulation/MasterSystem/Ports.cpp
@@ -0,0 +1,30 @@
+/*
+ * awui/Emulation/MasterSystem/Ports.cpp
+ *
+ * Copyright (C) 2014 Borja Sánchez Zamorano
+ */
+
+#include "Ports.h"
+
+using namespace awui::Emulation::MasterSystem;
+
+/*
+ *|-----------------------------------|
+ *| Port |   Input   |     Output     |
+ *|------|-----------|----------------|
+ *| 0x3E |           | Memory control |
+ *| 0x3F |      I/O port control      |
+ *| 0x7E | V counter |                |
+ *| 0x7F | H counter |       PSG      |
+ *| 0xBE |           |   VDP (data)   |
+ *| 0xBF |           |  VDP (control) |
+ *| 0xDC |         Controllers        |
+ *| 0xDD |         Controllers        |
+ *|-----------------------------------|
+ */
+
+Ports::Ports() {
+}
+
+Ports::~Ports() {
+}
---
+++ libawui/awui/Emulation/MasterSystem/Ports.h
@@ -0,0 +1,16 @@
+#ifndef _AWUI_EMULATION_MASTERSYSTEM_PORTS_H
+#define _AWUI_EMULATION_MASTERSYSTEM_PORTS_H
+
+namespace awui {
+	namespace Emulation {
+		namespace MasterSystem {
+			class Ports {
+				public:
+					Ports();
+					~Ports();
+			};
+		}
+	}
+}
+
+#endif
