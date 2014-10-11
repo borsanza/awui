e20039b127bc8205ee355c0488c3685cbe418298
BorSanZa <borsanza@gmail.com>
2014-10-11 22:37:50 +0200

    SliderBrowser

---
+++ libawui/awui/Windows/Forms/SliderBrowser.cpp
@@ -0,0 +1,15 @@
+/*
+ * awui/Windows/Forms/SliderBrowser.cpp
+ *
+ * Copyright (C) 2013 Borja Sánchez Zamorano
+ */
+
+#include "SliderBrowser.h"
+
+using namespace awui::Windows::Forms;
+
+SliderBrowser::SliderBrowser() {
+}
+
+SliderBrowser::~SliderBrowser() {
+}
---
+++ libawui/awui/Windows/Forms/SliderBrowser.h
@@ -0,0 +1,17 @@
+#ifndef _AWUI_WINDOWS_FORMS_SLIDERBROWSER_H
+#define _AWUI_WINDOWS_FORMS_SLIDERBROWSER_H
+
+#include <awui/Windows/Forms/Control.h>
+
+namespace awui {
+	namespace Windows {
+		namespace Forms {
+			class SliderBrowser : public Control {
+				SliderBrowser();
+				virtual ~SliderBrowser();
+			};
+		}
+	}
+}
+
+#endif
