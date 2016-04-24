337d97bb90ca06d2d4a4432780eb9ddf8414a230
BorSanZa <borsanza@gmail.com>
2016-04-24 13:43:17 +0200

    Actualizando valgrind

--- samples/awMasterGear/valgrind.sh
+++ samples/awMasterGear/valgrind.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 #vblank_mode=0 ./awMasterGear tests/*
-valgrind --tool=callgrind ./awMasterGear ./tests/64*.sms
+valgrind --tool=callgrind ./awMasterGear --test zexall/zexall_sdsc.sms
+
+# Despues ejecutamos
+# callgrind_annotate callgrind.out.<pid>
+
