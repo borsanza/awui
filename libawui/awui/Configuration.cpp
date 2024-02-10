cb255cb61072b4d4f4f9750fbecffe39f5e5f91a
Borja Sanchez Zamorano <devnull@localhost>
2024-02-10 19:39:36 +0100

    Creada clase para configuracion

---
+++ libawui/awui/Configuration.cpp
@@ -0,0 +1,132 @@
+/**
+ * awui/Configuration.cpp
+ *
+ * Copyright (C) 2024 Borja Sánchez Zamorano
+ */
+#include "Configuration.h"
+
+#include <awui/Console.h>
+
+#include <fstream>
+
+using namespace awui;
+
+awui::Configuration& Configuration::getInstance(const String &filePath) {
+	std::string filePathStr = filePath.ToCharArray();
+	static std::map<std::string, std::shared_ptr<Configuration>> instances;
+	if (instances.find(filePathStr) == instances.end()) {
+		auto instance = std::shared_ptr<Configuration>(new Configuration(filePath));
+		if (instance->Load(filePath)) {
+			instances[filePathStr] = instance;
+		} else {
+			Console::Error->WriteLine(String("Unable to load configuration from ") + filePath);
+		}
+	}
+
+	return *instances[filePathStr];
+}
+
+Configuration::Configuration(const String &filePath) {
+	m_currentFilePath = filePath;
+}
+
+bool Configuration::Load(const String &filePath) {
+	std::ifstream file(filePath.ToCharArray());
+	if (file.is_open()) {
+		file >> m_configJson;
+		return true;
+	} else {
+		std::ofstream outFile(filePath.ToCharArray());
+		if (outFile) {
+			nlohmann::json defaultConfig = { };
+			outFile << defaultConfig.dump(4);
+			outFile.close();
+			m_configJson = defaultConfig;
+			return true;
+		}
+	}
+
+	return false;
+}
+
+void Configuration::Save() {
+	if (m_currentFilePath.GetLength() > 0) {
+		std::ofstream file(m_currentFilePath.ToCharArray());
+		file << m_configJson.dump(4);
+	}
+}
+
+std::string Configuration::Read(const std::string &key, const std::string &defaultValue) {
+	if (m_configJson.contains(key)) {
+		return m_configJson[key].get<std::string>();
+	}
+
+	Write(key, defaultValue);
+
+	return defaultValue;
+}
+
+int64_t Configuration::Read(const std::string &key, const int64_t defaultValue) {
+	if (m_configJson.contains(key)) {
+		return m_configJson[key].get<int64_t>();
+	}
+
+	Write(key, defaultValue);
+
+	return defaultValue;
+}
+
+uint64_t Configuration::Read(const std::string &key, const uint64_t defaultValue) {
+	if (m_configJson.contains(key)) {
+		return m_configJson[key].get<uint64_t>();
+	}
+
+	Write(key, defaultValue);
+
+	return defaultValue;
+}
+
+bool Configuration::Read(const std::string &key, const bool defaultValue) {
+	if (m_configJson.contains(key)) {
+		return m_configJson[key].get<bool>();
+	}
+
+	Write(key, defaultValue);
+
+	return defaultValue;
+}
+
+double Configuration::Read(const std::string &key, const double defaultValue) {
+	if (m_configJson.contains(key)) {
+		return m_configJson[key].get<double>();
+	}
+
+	Write(key, defaultValue);
+
+	return defaultValue;
+}
+
+void Configuration::Write(const std::string &key, const std::string &value) {
+	m_configJson[key] = value;
+	Save();
+}
+
+void Configuration::Write(const std::string &key, const bool value) {
+	m_configJson[key] = value;
+	Save();
+}
+
+void Configuration::Write(const std::string &key, const int64_t value) {
+	m_configJson[key] = value;
+	Save();
+}
+
+void Configuration::Write(const std::string &key, const uint64_t value) {
+	m_configJson[key] = value;
+	Save();
+}
+
+void Configuration::Write(const std::string &key, const double value) {
+	m_configJson[key] = value;
+	Save();
+}
---
+++ libawui/awui/Configuration.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <nlohmann/json.hpp>
+
+#include <string>
+#include <awui/String.h>
+
+namespace awui {
+	class String;
+
+	class Configuration {
+	private:
+		nlohmann::json m_configJson;
+		String m_currentFilePath;
+
+		Configuration(const String &filePath);
+
+		bool Load(const String &filePath);
+
+		void Save();
+
+		// Prevent copying and assignment
+		Configuration(const Configuration&) = delete;
+		Configuration& operator=(const Configuration&) = delete;
+
+	public:
+		static Configuration& getInstance(const String & filePath);
+
+		std::string Read(const std::string &key, const std::string &defaultValue);
+		bool        Read(const std::string &key, const bool         defaultValue);
+		int64_t     Read(const std::string &key, const int64_t      defaultValue);
+		uint64_t    Read(const std::string &key, const uint64_t     defaultValue);
+		double      Read(const std::string &key, const double       defaultValue);
+
+		void Write(const std::string &key, const std::string &value);
+		void Write(const std::string &key, const bool         value);
+		void Write(const std::string &key, const int64_t      value);
+		void Write(const std::string &key, const uint64_t     value);
+		void Write(const std::string &key, const double       value);
+	};
+}
