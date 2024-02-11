6b9ae6675987cc0dc7484a9ef6b47e1371b0d7f9
Borja Sanchez Zamorano <devnull@localhost>
2024-02-11 11:44:22 +0100

    Borrado de dlls

--- clean32.bat
+++ clean32.bat
@@ -10,4 +10,11 @@ del CMakeCache.txt 2>nul
 if exist build.ninja ninja -t clean
 del build.ninja 2>nul
 
+del libawui\*.dll 2> nul
+del libawui\*.dll.a 2> nul
+del samples\awSlider\*.dll 2> nul
+del samples\awTest\*.dll 2> nul
+del samples\awuiDemo\*.dll 2> nul
+del samples\stationTV\*.dll 2> nul
+
 endlocal
